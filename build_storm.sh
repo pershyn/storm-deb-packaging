389a301f8069c513709f71a1992c5711cb462ea4
Thomas Dudziak <tomdzk@gmail.com>
2012-12-04 10:25:48 -0800

    Tweaked storm build

--- build_storm.sh
+++ build_storm.sh
@@ -2,43 +2,43 @@
 name=storm
 version=0.8.1
 url=http://storm-project.net
+package="storm-${version}.zip"
+download_url="https://github.com/downloads/nathanmarz/storm/${package}"
 buildroot=build
 fakeroot=storm-${version}
 origdir="$(pwd)"
 description="Storm is a distributed realtime computation system. Similar to how Hadoop provides a set of general primitives for doing batch processing, Storm provides a set of general primitives for doing realtime computation. Storm is simple, can be used with any programming language, is used by many companies, and is a lot of fun to use!"
+storm_root_dir=/usr/lib/storm
 
 #_ MAIN _#
 rm -rf ${name}*.deb
+if [[ ! -f "${package}" ]]; then
+  wget ${download_url}
+fi
+mkdir -p tmp && pushd tmp
 rm -rf ${fakeroot}
-mkdir -p ${fakeroot}
-wget https://github.com/downloads/nathanmarz/storm/storm-${version}.zip
-mv storm-${version}.zip ${fakeroot}
-unzip ${fakeroot}/storm-${version}.zip
-mv ${fakeroot}/storm-${version}/* ${fakeroot} &>/dev/null
-rm -rf ${fakeroot}/storm-${version}.zip
+unzip ${origdir}/storm-${version}.zip
 rm -rf ${fakeroot}/logs
 rm -rf ${fakeroot}/log4j
 rm -rf ${fakeroot}/conf
 
 #_ MAKE DIRECTORIES _#
 rm -rf ${buildroot}
-mkdir -p ${buildroot}
-mkdir -p ${buildroot}/opt/storm
+mkdir -p ${buildroot}${storm_root_dir}
 mkdir -p ${buildroot}/etc/default
 mkdir -p ${buildroot}/etc/storm/conf.d
 mkdir -p ${buildroot}/etc/init
 mkdir -p ${buildroot}/var/log/storm
-mkdir -p ${buildroot}/var/lib/storm
 
 #_ COPY FILES _#
-cp -R ${fakeroot}/* ${buildroot}/opt/storm
-cp storm storm-nimbus storm-supervisor storm-ui storm-drpc ${buildroot}/etc/default
-cp storm.yaml ${buildroot}/etc/storm
-cp storm.log.properties ${buildroot}/etc/storm
-cp storm-nimbus.conf storm-supervisor.conf storm-ui.conf storm-drpc.conf ${buildroot}/etc/init
+cp -R ${fakeroot}/* ${buildroot}${storm_root_dir}
+cp ${origdir}/storm ${origdir}/storm-nimbus ${origdir}/storm-supervisor ${origdir}/storm-ui ${origdir}/storm-drpc ${buildroot}/etc/default
+cp ${origdir}/storm.yaml ${buildroot}/etc/storm
+cp ${origdir}/storm.log.properties ${buildroot}/etc/storm
+cp ${origdir}/storm-nimbus.conf ${origdir}/storm-supervisor.conf ${origdir}/storm-ui.conf ${origdir}/storm-drpc.conf ${buildroot}/etc/init
 
 #_ MAKE DEBIAN _#
 cd ${buildroot}
 fpm -t deb -n $name -v $version --description "$description" --url="$url" -a all --prefix=/ -d "libzmq0 = 2.1.7" -d "jzmq >= 2.1.0" -s dir -- .
-mv ${origdir}/${buildroot}/*.deb ${origdir}
-cd ${origdir}
+mv storm*.deb ${origdir}
+popd
