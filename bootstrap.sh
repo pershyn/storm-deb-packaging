ab60e2582985758b3bbe832e162b12a4f5993a67
Michael Pershyn <michael.pershyn@gmail.com>
2014-06-17 12:22:08 +0200

    added vagrant environment that rely on wheezy box

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+sudo apt-get update
+sudo apt-get install --fix-missing -y git g++ uuid-dev ruby ruby-dev  make wget curl libtool openjdk-6-jdk pkg-config autoconf automake unzip
+
+#TODO:
+#export JAVA_HOME=/usr/lib/jvm/java-6-openjdk
+
+sudo gem install fpm
+
