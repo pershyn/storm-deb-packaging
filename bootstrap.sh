368bf8530fee654be16f3e7710596dc535f279cd
Michael Pershyn <michael.pershyn@gmail.com>
2014-07-25 13:58:55 +0200

    dropped some more stuff

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,3 @@
 sudo apt-get update
-sudo apt-get install --fix-missing -y git g++ uuid-dev ruby ruby-dev  make wget curl libtool openjdk-6-jdk pkg-config autoconf automake unzip dpkg-dev fakeroot debhelper
 
-#TODO:
-#export JAVA_HOME=/usr/lib/jvm/java-6-openjdk
-
-sudo gem install fpm
+sudo apt-get install --fix-missing -y git g++ uuid-dev make wget libtool openjdk-6-jdk pkg-config autoconf automake unzip dpkg-dev fakeroot debhelper
